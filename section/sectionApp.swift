525a5bbb68bc6ac17791938913bd5d1adc7d63ca
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-22 10:40:40 -0700

    Initial Commit

---
+++ section/sectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  sectionApp.swift
+//  section
+//
+//  Created by user on 6/22/21.
+//
+
+import SwiftUI
+
+@main
+struct sectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
